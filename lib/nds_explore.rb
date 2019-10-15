52c8d6e0641bbda60fc47f4ea9ee198bf3e9418f
Dominic Chu <dominic.m.chu@gmail.com>
2019-10-15 17:28:52 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,19 +11,5 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  array_directors_database = directors_database
-  first_index = 0
-  while first_index < array_directors_database.count do
-    if array_directors_database[first_index][:name] == "Steven Spielberg"
-    second_index = 0
-    while second_index < array_directors_database[first_index].count do
-      third_index = 0
-      while third_index < array_directors_database[first_index][second_index].count do
-        puts array_directors_database[first_index][second_index][third_index][:title]
-        third_index += 1
-      end
-      second_index += 1
-    end
-    first_index += 1
-  end
+
 end
