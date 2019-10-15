9f070cf962f1a8d25407322dc2378e68a2ce6615
Dominic Chu <dominic.m.chu@gmail.com>
2019-10-15 17:35:59 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,5 +12,10 @@ end
 
 def print_first_directors_movie_titles
   new_var = directors_database
-  puts new_var[0][:movies][0][:title]
+  # puts new_var[0][:movies][0][:title]
+  index_1 = 0
+  while index_1 < directors_database[0][:movies].count do
+    puts directors_database[0][:movies][index_1][:title]
+    index_1 += 1
+  end
 end
